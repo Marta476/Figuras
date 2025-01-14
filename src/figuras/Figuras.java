369a708976c7630fc0bbfb79ffabdc0408746d0b
marta.burgaleta <marta.burgaleta@A15-PC26.JUANXXIII.net>
2025-01-14 15:57:26 +0100

    creacion de la clase figuras

---
+++ src/figuras/Figuras.java
@@ -0,0 +1,17 @@
+package figuras;
+
+/**
+ *
+ * @author marta.burgaleta
+ */
+public class Figuras {
+
+    /**
+     * Creación de la 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
