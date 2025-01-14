972edd925a5849098be0cb8f82b7175f50c65eb0
marta.burgaleta <marta.burgaleta@A15-PC26.JUANXXIII.net>
2025-01-14 17:19:51 +0100

    calcular area y perimetro del radio del circulo

--- src/figuras/Circulo.java
+++ src/figuras/Circulo.java
@@ -6,9 +6,34 @@ package figuras;
  */
 public class Circulo {
     private int radio;
-
-    public Circulo(int radio) {
+    
+    /**
+     * Constructor de la clase circulo
+     * @param radio valor del radio
+     */
+    public Circulo(int radio){
+        this.radio = radio;
+    }
+    /**
+     * Obtiene el valor del radio del circulo
+     * @param radio El valor del radio del circulo
+     */
+    public int getRadio() {
+        return radio;
+    }
+    /**
+     * Establece un valor para el radio del circulo
+     * @return El valor del radic del circulo
+     */
+    public void setRadio(int radio) {
         this.radio = radio;
     }
     
+    public double CalcularArea(){
+        return pi*radio*radio;
+    }
+
+    public double calcularPerimetro(){
+        return 2*pi*radio;
+    }
 }
